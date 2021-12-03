c98a430aa1969505115d2725a241c6794d009a6f
Max Wagner <Maximilian.Wagner@cas.de>
2021-12-03 19:06:30 +0100

    Running out of time for day 3 b. Oh well.

--- aoc03/src/main.rs
+++ aoc03/src/main.rs
@@ -7,17 +7,68 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
-    let mut line_cnt = 0;
-    
-    let mut one_counts: Vec<i32> = Vec::new();
+
+    // we need a different data format and I'm too stripped for time to rewrite the old code, so hackery it is!
+    let mut matrix: Vec<String> = Vec::new();
     for line in read_lines(Path::new(filename)) {
-        count_ones(&mut one_counts, line.unwrap());
-        line_cnt += 1;
+        matrix.push(line.unwrap());
     }
 
+    let mut o2plus = matrix.clone();
+    let mut bit = 0;
+    while o2plus.len() > 1 {
+        o2plus = do_search_round(&mut o2plus, 0, bit);
+        bit += 1;
+    }
+
+    let mut co2minus = matrix.clone();
+    bit = 0;
+    while co2minus.len() > 1 {
+        co2minus = do_search_round(&mut co2minus, 1, bit);
+        bit += 1;
+    }
+
+    println!("{:}", o2plus[0]);
+    println!("{:}", co2minus[0]);
+    
+    let o2plus_num = isize::from_str_radix(&o2plus[0], 2).unwrap();
+    let co2minus_num = isize::from_str_radix(&co2minus[0], 2).unwrap();
+    println!("Life Support Rating: {:}", co2minus_num * o2plus_num);
+}
+
+fn do_search_round(mx: &mut Vec<String>, which_pattern: usize, bit: usize) -> Vec<String> {
+    let mut next = Vec::new();
+    let tuple = gamma_epsilon(mx);
+    let pattern = if which_pattern == 0 { tuple.0 } else { tuple.1 };
+    for candidate in mx.iter() {
+        if candidate_matches(&candidate, &pattern, bit) {
+            next.push(candidate.clone());
+        }
+    }
+    next
+}
+
+fn candidate_matches(cand: &String, pattern: &String, bit: usize) -> bool {
+    cand.chars().nth(bit) == pattern.chars().nth(bit)
+}
+
+fn gamma_epsilon(mx: &mut Vec<String>) -> (String, String) {
+    let mut line_cnt = 0;
+
+    let mut one_counts: Vec<i32> = Vec::new();
+    for line in mx.iter() {
+        count_ones(&mut one_counts, line);
+        line_cnt += 1;
+    }
+    
     let mut gamma = String::new();
     let mut epsilon = String::new();
-    for (i, one_count) in one_counts.iter().enumerate() {
+    for one_count in one_counts.iter() {
+        if one_count == &(line_cnt / 2) {
+            gamma.push('1');
+            epsilon.push('0');
+            continue;
+        }
         let next = match one_count > &(line_cnt / 2) {
             true => '1',
             false => '0',
@@ -30,18 +81,11 @@ fn main() {
         };
         epsilon.push(next);
     }
-    println!("{}", gamma);
-    println!("{}", epsilon);
-
-    let gamma_num = isize::from_str_radix(&gamma, 2).unwrap();
-    let epsilon_num = isize::from_str_radix(&epsilon, 2).unwrap();
-    println!("{:}", gamma_num);
-    println!("{:}", epsilon_num);
-    println!("Power consumption: {:}", gamma_num * epsilon_num);
-    
+    (gamma, epsilon)
 }
 
-fn count_ones(one_counts: &mut Vec<i32>, line: String) {
+
+fn count_ones(one_counts: &mut Vec<i32>, line: &String) {
     if one_counts.len() < line.len() {
         for _ in 0..line.len() {
             one_counts.push(0);
